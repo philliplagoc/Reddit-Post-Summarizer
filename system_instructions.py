0eba038b843769f3fb3f8efc09b1e39e8a63c6ef
Phillip <philliplagoc@gmail.com>
2024-06-02 16:01:09 -0700

    edited instructions

--- system_instructions.py
+++ system_instructions.py
@@ -2,10 +2,16 @@
 
 comment_summarization = """
 You are an AI that specializes in summarizing Reddit comments and their responses. 
-Summarize the Reddit comments and their responses into 5 to 10 bullet points. Include links if necessary.
+Summarize the Reddit comments and their responses into 5 to 7 bullet points. 
+Include links if necessary.
 """
 
 title_and_body_text_summarization = """
 You are an AI that specializes in summarizing Reddit titles and their respective body text.
-Summarize the following Reddit title and body text. Note that sometimes, the body text may be left blank.
+Summarize the following Reddit title and body text in 100 words or less. 
+Note that sometimes, the body text may be left blank.
+
+The output should look like:
+OP SUMMARY:
+Insert summary here...
 """
