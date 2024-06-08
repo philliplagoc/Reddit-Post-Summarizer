727843c797eb10618708a0d3ed93ddf6bd02e7f8
Phillip <philliplagoc@gmail.com>
2024-06-08 15:41:15 -0700

    init helper functions

--- functions.py
+++ functions.py
@@ -43,14 +43,32 @@ def get_top_n_comments(comments, n=3):
     
     return '\n'.join(result)
 
-# Calculate cost of Claude message.
-def calculate_haiku_cost(message):
+# Calculate cost of message.
+def calculate_cost(message):
     input_tokens = message.usage.input_tokens
     output_tokens = message.usage.output_tokens
     input_cost = 0.25 * (input_tokens / 1_000_000)
     output_cost = 1.25 * (output_tokens / 1_000_000)
     total_cost = input_cost + output_cost
     print("This cost ${:.2f} + ${:.2f} = ${:.2f}.".format(input_cost, output_cost, total_cost))
+
+# Send message to OpenAI.
+def send_message(client, sys_instructions, user_query, model="gpt-3.5-turbo-0125"):
+    response = client.chat.completions.create(
+        model = model,
+        messages = [
+            {
+                "role": "system",
+                "content": sys_instructions
+            },
+            {
+                "role": "user",
+                "content": user_query
+            }
+        ]
+    )
+    return response
+
 
 # Send message to Claude Haiku.
 def send_message_to_claude(anthropic_client, sys_instructions, user_query, model="claude-3-haiku-20240307", max_tokens=4000):
